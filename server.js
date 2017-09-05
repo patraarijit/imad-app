887bfc461a2b97b08836f67f77469ab1ef526d88
patraarijit <31044472+patraarijit@users.noreply.github.com>
2017-09-05 22:36:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-one',function(req,res){
  res.sendFile(path.join('Article one serve the page'));   
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send(__dirname, 'ui', 'style.css');
 });
 
 app.get('/ui/madi.png', function (req, res) {
